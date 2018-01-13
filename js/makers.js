74df423ce092b846e5a78bef5d10729c1638a3f0
Ilyar Mnazhdin <ilyarmnzhdn@gmail.com>
2018-01-13 17:17:02 +0600

    Create makers.js

---
+++ js/makers.js
@@ -0,0 +1,44 @@
+var myLocations = [
+    {
+        title: 'William Lawsons Bar',
+        lat: 43.2537422,
+        lng: 76.9547127,
+        type: 'Bar'
+    },
+    {
+        title: 'Barmaglot',
+        lat: 43.2396586,
+        lng: 76.9546756,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Ziyafet Steakehouse',
+        lat: 43.2387854,
+        lng: 76.9421418,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Nedelka',
+        lat: 43.2433634,
+        lng: 76.953444,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Daily Coffee',
+        lat: 43.2428991,
+        lng: 76.9425746,
+        type: 'Coffee'
+    },
+    {
+        title: 'Marrone Rosso',
+        lat: 43.248032,
+        lng: 76.9473501,
+        type: 'Coffee'
+    },
+    {
+        title: 'Rixos Hotel and Spa',
+        lat: 43.249413,
+        lng: 76.934591,
+        type: 'Hotel'
+    }
+]
