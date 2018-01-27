29ae44a3e78013fc354e72228bce90a75439c667
Ilyar Mnazhdin <ilyarmnzhdn@gmail.com>
2018-01-27 12:47:03 +0600

    Create locations.js

---
+++ js/locations.js
@@ -0,0 +1,11 @@
+    // These are the real estate listings that will be shown to the user.
+    // This is my data model
+var locations = [
+      {title: 'William Lawsons Bar', location: {lat: 43.2537422, lng: 76.9547127}},
+      {title: 'Barmaglot', location: {lat: 43.2396586, lng: 76.9546756}},
+      {title: 'Ziyafet Steakehouse', location: {lat: 43.2387854, lng: 76.9421418}},
+      {title: 'Nedelka', location: {lat: 43.2433634, lng: 76.953444}},
+      {title: 'Daily Coffee', location: {lat: 43.2428991, lng: 76.9425746}},
+      {title: 'Marrone Rosso', location: {lat: 43.248032, lng: 76.9473501}},
+      {title: 'Rixos Hotel and Spa', location: {lat: 43.249413, lng: 76.934591}}
+];
